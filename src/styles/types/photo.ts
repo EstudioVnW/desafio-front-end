1c60d67798a1ca24e15815df1f7f53a0f20933f7
menezesvs <aline.vsousa@hotmail.com>
2025-07-10 11:06:49 -0300

    Corrigindo alguns erros

--- src/styles/types/photo.ts
+++ src/styles/types/photo.ts
@@ -1,13 +1,13 @@
 export interface Photo {
-  id: string
-  alt_description: string | null
+  id: string;
+  alt_description: string | null;
   urls: {
-    full: string
-    regular: string
-    small: string
-  }
+    full: string;
+    regular: string;
+    small: string;
+  };
   user: {
-    name: string
-    portfolio_url: string | null
-  }
+    name: string;
+    portfolio_url: string | null;
+  };
 }
--- src/styles/utils/api.ts
+++ src/styles/utils/api.ts
@@ -2,10 +2,24 @@ import { Photo } from "../../types/photo";
 
 const ACCESS_KEY = "9a5cwvCMg03m26bMRh7D7pTiRwPX6_X5P_33bmY_Ops";
 
+interface UnsplashAPIResponse {
+  id: string;
+  alt_description: string;
+  urls: {
+    small: string;
+    regular?: string;
+    full: string;
+  };
+  user: {
+    name: string;
+    portfolio_url?: string | null;
+  };
+}
+
 export async function fetchPhotos(query = "random"): Promise<Photo[]> {
   const res = await fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=12&client_id=${ACCESS_KEY}`);
   const data = await res.json();
-  return data.results.map((item: any) => ({
+  return data.results.map((item: UnsplashAPIResponse) => ({
     id: item.id,
     alt_description: item.alt_description,
     urls: {
